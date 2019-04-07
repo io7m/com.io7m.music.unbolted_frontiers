5309884bd5789a318c65e0339ea4eaddcdf23f4a
Mark Raynsford <code@io7m.com>
2019-04-07 15:41:48 +0100

    Mark 0.0.3 in makefont.sh

--- makefont.sh
+++ makefont.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 exec java \
-  -jar target/com.io7m.unbolted_frontiers-0.0.2-main.jar \
+  -jar target/com.io7m.unbolted_frontiers-0.0.3-main.jar \
   --input-directory src/main/flac \
   --input-percussion-directory src/main/flac_perc \
-  --output-file unbolted-0.0.2.sf2
+  --output-file unbolted-0.0.3.sf2
 
